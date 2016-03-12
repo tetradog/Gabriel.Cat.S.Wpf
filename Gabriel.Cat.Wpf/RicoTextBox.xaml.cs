ec23f6ebf38f5a0bfcf77c55929e041830539369
tetradog <tetradog@gmail.com>
2016-03-12 18:03:25 +0100

    RicoTextBox control para usar el RichTextBox de forma simple y usando el menuContextual para realizar sus funciones sobre el texto

---
+++ Gabriel.Cat.Wpf/RicoTextBox.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Gabriel.Cat.Extension;
+using System.Xml;
+using System.Threading;
+
+namespace Gabriel.Cat.Wpf
+{
+    /// <summary>
+    /// Lógica de interacción para RicoTextBox.xaml
+    /// </summary>
+    public partial class RicoTextBox : UserControl
+    {
+        public event TextChangedEventHandler TextoCambiado;
+        Tiket<TextChangedEventArgs> eventoTextoCambiado;
+        public RicoTextBox()
+        {
+            InitializeComponent();
+            imgCopiar.SetImage(Resource1.copiar);
+            imgPegar.SetImage(Resource1.pegar);
+            imgCortar.SetImage(Resource1.cortar);
+            eventoTextoCambiado = new Tiket<TextChangedEventArgs>(()=> { });
+        }
+        public string Text
+        {
+            get
+            {
+                return rtText.GetText();
+            }
+            set
+            {
+
+                if (value == null)
+                    value = "";
+
+                rtText.SetText(value);
+            }
+        }
+        public string TextWithFormat
+        {
+            get
+            {
+                return rtText.ToStringRtf();
+            }
+            set
+            {
+                rtText.LoadRtf(value);
+            }
+        }
+
+        private void rtText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            eventoTextoCambiado.AbortaTrabajo();
+            eventoTextoCambiado = new Tiket<TextChangedEventArgs>((o) =>
+            {
+                Thread.Sleep(1000);//espero un segundo
+                if (TextoCambiado != null)
+                    TextoCambiado(sender, e);
+            }, null);
+            eventoTextoCambiado.HazFaenaAsync();
+
+        }
+    }
+}
