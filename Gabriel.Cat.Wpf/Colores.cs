b50e448cba6780ee1d8bec20d87ae7d686a237cb
tetradog <tetradog@gmail.com>
2016-03-15 18:28:03 +0100

    para poder obtener los colores y sus nombres facilmente :)

---
+++ Gabriel.Cat.Wpf/Colores.cs
@@ -0,0 +1,42 @@
+﻿using Gabriel.Cat.Extension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Gabriel.Cat.Wpf
+{
+    public static class Colores
+    {
+        static TwoKeysList<string, string, System.Windows.Media.Color> colores;
+        public static readonly System.Windows.Media.Color[] ListaColores;
+        static Colores()
+        {
+            colores = new TwoKeysList<string, string, System.Windows.Media.Color>();
+            System.Reflection.PropertyInfo[] pinfo = typeof(System.Windows.Media.Colors).GetProperties();
+            System.Windows.Media.Color color;
+            for (int i = 0; i < pinfo.Length; i++)
+            {
+                try {
+                    color = (System.Windows.Media.Color)pinfo[i].GetGetMethod().Invoke(null, null);
+                    colores.Add(pinfo[i].Name, color.ToString(), color);
+                }
+                catch { }
+            }
+            ListaColores = colores.ValuesToArray();
+        }
+        public static IEnumerable<System.Windows.Media.Color> GetColors()
+        {
+            return ListaColores;
+        }
+        public static string GetName(System.Windows.Media.Color color)
+        {
+            return colores.ObtainTkey1WhithTkey2(color.ToString());
+        }
+
+
+    }
+}
