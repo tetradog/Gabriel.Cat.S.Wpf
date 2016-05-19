725fe32b17b159cce451165feebbdcad10d5c081
tetraadog <tetradog@gmail.com>
2016-05-19 04:22:35 +0200

    commit

--- Gabriel.Cat.Wpf/ControlesDeInternet/ColorPicker/ColorPicker.xaml.cs
+++ Gabriel.Cat.Wpf/ControlesDeInternet/ColorPicker/ColorPicker.xaml.cs
@@ -80,6 +80,7 @@ namespace WPFColorPickerLib
                     CreateAlphaLinearBrush();
                     UpdateTextBoxes();
                     UpdateInk();
+                    
                 }
             }
         }
