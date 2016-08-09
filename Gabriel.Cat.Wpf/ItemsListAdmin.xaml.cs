0f587e4471171e1d8603bedeea70aad2d71d0d5e
tetraadog <tetradog@gmail.com>
2016-08-10 00:58:15 +0200

    subir controles

---
+++ Gabriel.Cat.Wpf/ItemsListAdmin.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gabriel.Cat.Wpf
+{
+    /// <summary>
+    /// Interaction logic for ItemsListAdmin.xaml
+    /// </summary>
+    public partial class ItemsListAdmin : UserControl
+    {
+        public ItemsListAdmin()
+        {
+            InitializeComponent();
+        }
+        public MenuItems Menu
+        {
+            get { return menu; }
+        }
+        public StackPanel StackItems
+        {
+            get { return stkItems; }
+        }
+    }
+}
---
+++ Gabriel.Cat.Wpf/MenuItems.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Gabriel.Cat.Extension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gabriel.Cat.Wpf
+{
+    /// <summary>
+    /// Interaction logic for ItemsAdmin.xaml
+    /// </summary>
+    public partial class MenuItems : UserControl
+    {
+        public event EventHandler ClickLeftButton;
+        public event EventHandler ClickCenterButton;
+        public event EventHandler ClickRightButton;
+        public MenuItems()
+        {
+            InitializeComponent();
+           
+        }
+        public ToggleButton Left
+        {
+            get { return tgLeft; }
+        }
+        public ToggleButton Right
+        {
+            get { return tgRight; }
+        }
+        public ToggleButton Center
+        {
+            get { return tgCenter; }
+        }
+        private void tgLeft_ChangeIndex(object sender, ToggleButtonArgs e)
+        {
+            if (ClickLeftButton != null)
+                ClickLeftButton(this, new EventArgs());
+        }
+
+        private void tgCenter_ChangeIndex(object sender, ToggleButtonArgs e)
+        {
+            if (ClickCenterButton != null)
+                ClickCenterButton(this, new EventArgs());
+        }
+
+        private void tgRight_ChangeIndex(object sender, ToggleButtonArgs e)
+        {
+            if (ClickRightButton != null)
+                ClickRightButton(this, new EventArgs());
+        }
+    }
+}
