348af5150da90f867767d5a0b3369fa3e28538dc
Gabriel <tetradog@gmail.com>
2018-04-11 14:02:04 +0200

    reubicando clases y añadiendo la clase Aplicacion para tratar el inicio de windows :D

---
+++ Gabriel.Cat.Wpf/Aplicacion.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32;
+using System.Windows.Forms;
+/*
+Creado por tttony 2010
+http://tttony.blogspot.com/
+
+*/
+namespace Gabriel.Cat
+{
+
+    public static class Aplicacion
+    {
+        public static bool EstaEnElInicio
+        {
+            get
+            {
+
+                string path;
+                string thisApp;
+                RegistryKey hklm = Registry.LocalMachine;
+
+                hklm = hklm.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");//solo sirve en windows...
+                path = hklm.GetValue(Application.ProductName).ToString();
+
+                thisApp = Application.ExecutablePath.ToUpper();
+                hklm.Close();
+
+                return path != thisApp;
+            }
+            set
+            {
+                RegistryKey hklm = Registry.LocalMachine;
+                string thisApp;
+
+                hklm = hklm.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+
+                if (value)
+                {
+                    thisApp = Application.ExecutablePath.ToUpper();
+                    hklm.SetValue(Application.ProductName, thisApp);
+                }
+                else
+                {
+                    //delete registro
+                    hklm.DeleteValue(Application.ProductName);
+                }
+
+                hklm.Close();
+            }
+        }
+    }
+}
